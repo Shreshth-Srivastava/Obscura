aac0c780cbbf76de98a352c223799ca8a73e99b3
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2026-01-12 20:55:16 +0530

    Configured Resend's email credentials

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-        from: 'you@example.com',
+        from: 'Acme <onboarding@resend.dev>',
         to: email,
         subject: 'Obscura Verification Code',
         react: VerificationEmail({username, otp: verifyCode}),
