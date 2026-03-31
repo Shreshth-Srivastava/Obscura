e3401306a2d6bf4a3b0d1dd6b369a97e9ea25cbf
Shreshth Srivastava <shreshthsrivastava7679@gmail.com>
2026-03-31 21:36:05 +0530

    Frontend - SignIn

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,21 +1,157 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import * as e from "zod";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { signInSchema } from "@/schemas/signInSchema";
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { signIn } from "next-auth/react";
+
+const page = () => {
+  const [username, setUsername] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const router = useRouter();
+
+  // zod implementation
+  const form = useForm<e.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "", 
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: e.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        identifier: data.identifier,
+        password: data.password
+      });
+      if(result?.error){
+        toast.error("Login Failed");
+      }
+      if(result?.url){
+        router.replace('/dashboard');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Login Failed");
+    } finally{
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-white text-black py-1 px-4" onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md mx-4">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Obscura
+          </h1>
+          <p className="mb-4">Sign In to start your anonymus adventure</p>
+        </div>
+        <form
+          id="form-rhf-demo"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-x-6"
+        >
+          <FieldGroup>
+            <Controller
+              name="identifier"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">
+                    Email/Username
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-username"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Email/Username"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">
+                    Password
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    id="form-rhf-demo-password"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Password"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+          <Field className="mt-4">
+            <Button variant="default" type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                </>
+              ) : (
+                "Sign-In"
+              )}
+            </Button>
+          </Field>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
+
+
+// import { useSession, signIn, signOut } from "next-auth/react"
+
+// export default function Component() {
+//   const { data: session } = useSession()
+//   if (session) {
+//     return (
+//       <>
+//         Signed in as {session.user.email} <br />
+//         <button onClick={() => signOut()}>Sign out</button>
+//       </>
+//     )
+//   }
+//   return (
+//     <>
+//       Not signed in <br />
+//       <button className="bg-white text-black py-1 px-4" onClick={() => signIn()}>Sign in</button>
+//     </>
+//   )
+// }
---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+const MsgPage = () => {
+  return (
+    <div>
+      MsgPage
+    </div>
+  )
+}
+
+export default MsgPage
